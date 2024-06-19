cf94c7b1c0749798d227c84a3fa6b39d3c4c2eba
Von P. Walden <vonw@gaia.localdomain>
2024-06-19 08:55:23 -0700

    Update to commit and push daily trajectory plots to github

--- raven-trajectories.sh
+++ raven-trajectories.sh
@@ -5,7 +5,9 @@ cd /home/vonw/work/software/icecaps-summit/back-trajectories
 /home/vonw/anaconda3/envs/work/bin/python create-raven-trajectories.py
 /home/vonw/anaconda3/envs/work/bin/python raven-trajectory-plots.py
 
-#git add .
-#d=$(date -d "yesterday" -u -I)
-#git commit -m "Daily trajectories for ${d}"
-#git push
+eval (ssh-agent -c)
+ssh-add ~/.ssh/id_rsa
+git add .
+d=$(date -d "yesterday" -u -I)
+git commit -m "Daily trajectories for ${d}"
+git push
