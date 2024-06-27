35ae39536866c4612886c7cea1575b8e3b5c48fa
Von P. Walden <vonw@gaia.localdomain>
2024-06-27 15:31:29 -0700

    Daily trajectories for 2024-06-26

--- create-raven-trajectories.py
+++ create-raven-trajectories.py
@@ -2,6 +2,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from multiprocessing import Pool
 
 # ....Import hysplit
 sys.path.append('/home/vonw/work/software/backTrajectories/')
@@ -15,11 +16,10 @@ def download_GFS_data(date):
     hy.retrieveGFSdataFromNOAA()
     return
 
-def generate_back_trajectories(date):
-    # ....Create hysplit back trajectories for yesterday
-    times = pd.date_range(date.strftime('%Y-%m-%d')+' 00:00', date.strftime('%Y-%m-%d')+' 23:00', freq='1h')
+def generate_back_trajectories(times):
+    # ....Create hysplit back trajectories for particular time 
     dataSource = 'GFS'
-    length = 120
+    length = 120    # 120 hours = 5 days
     lat    = +66.5
     lon    = -46.3
     alts   = np.arange(100, 10000, 500)
@@ -29,9 +29,9 @@ def generate_back_trajectories(date):
     return
 
 # %%
-date = pd.Timestamp.now()
-datem1 = (date - pd.Timedelta('1 days'))
+today     = pd.Timestamp.now()
+yesterday = today - pd.Timedelta('1 days')
+times = pd.date_range(yesterday.strftime('%Y-%m-%d')+' 00:00', yesterday.strftime('%Y-%m-%d')+' 23:00', freq='1h')
 
-download_GFS_data(date)
-generate_back_trajectories(datem1)
-
+download_GFS_data(today)
+generate_back_trajectories(times)
