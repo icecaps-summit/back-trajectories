a5b3591a2ae7216d14250294ebc5316c802834ba
Von P. Walden <vonw@gaia.localdomain>
2024-07-19 15:47:50 -0700

    Update to holoviz panel app for viewing trajectories

--- raven-panel.py
+++ raven-panel.py
@@ -1,4 +1,5 @@
 # %%
+import numpy as np
 import xarray as xr
 import hvplot.xarray
 import datetime as dt
@@ -9,48 +10,19 @@ pn.extension()
 raven_back_trajectories = xr.open_dataset('/mnt/disk2/data/hysplit/backTrajectories/raven_back_trajectories.nc')
 
 # %%
-def trajectory_plot(raven_back_trajectories, variable):
-    trj_plot = raven_back_trajectories.sel(time=trj_time.value, vertical_level=trj_levels.value)[variable].plot()
-    return trj_plot
+def trajectory_plot(variable='latitude', time=dt.datetime(2024, 5, 15, 0), level=100):
+    return raven_back_trajectories.sel(time=time, vertical_level=level, method='nearest')[variable].hvplot()
 
 # %%
-trj_levels = pn.widgets.IntRangeSlider(
-    name='Vertical Level of Trajectory (m)',
-    start=100, 
-    end=9100, 
-    value=(100, 100), 
-    step=500,
-    bar_color='green')
+variables = [variable for variable in raven_back_trajectories.variables][0:11]
+variable_widget = pn.widgets.Select(name='Trajectory Variable', options=variables)
+#time_widget = pn.widgets.DatetimePicker(name='Date of Trajectories', value=dt.datetime(2024, 5, 15), start=dt.datetime(2024, 5, 15), end = dt.datetime.today() - dt.timedelta(days=1))
+#time_widget = pn.widgets.DateSlider(name='Date of Trajectories', value=dt.datetime(2024, 5, 15, 0), start=dt.datetime(2024, 5, 15, 0), end = dt.datetime.today() - dt.timedelta(days=1))
+time_widget = pn.widgets.DiscreteSlider(name='Date of Trajectories', options=list(raven_back_trajectories.time.values), value=raven_back_trajectories.time[0].values, bar_color='green')
+level_widget = pn.widgets.IntSlider(name='Vertical Level of Trajectory (m)', start=100, end=9100, value=100, step=500, bar_color='green')
 
 # %%
-trj_time = pn.widgets.DatetimePicker(
-    name='Date of Trajectories', 
-    value=dt.datetime(2024, 5, 15),
-    start=dt.datetime(2024, 5, 15),
-    end = dt.datetime.today() - dt.timedelta(days=1)
-)
+plot = pn.bind(trajectory_plot, variable=variable_widget, time=time_widget, level=level_widget)
+widgets = pn.Column(variable_widget, time_widget, level_widget)
+pn.Column(widgets, plot).show()
 
-# %%
-import panel.widgets as pnw
-
-# %%
-trj_time  = pnw.IntSlider(name='time', start=0, end=len(raven_back_trajectories.time)-1)
-trj_level = pnw.IntSlider(name='level', start=0, end=len(raven_back_trajectories.vertical_level)-1)
-
-variables = ['altitude', 'pressure', 'potential temperature', 'air temperature', 'rainfall', 'mix depth', 'relative humidity', 'terrain above msl', 'solar flux']
-variable  = variables[3]
-raven_back_trajectories[variable].interactive(width=800).isel(time=trj_time, vertical_level=trj_level).hvplot(ylim=[220, 300])
-
-# %%
-x='longitude', y='latitude', geo=True, coastlines=True, xlim=[-90,0], ylim=[45,90])
-
-# %%
-colorScaleVariable = 'altitude'
-plot = raven_back_trajectories.sel(time='2024-05-15 00:00', method='nearest').hvplot.points(x='longitude', y='latitude', 
-                    geo=True, 
-                    c=raven_back_trajectories[colorScaleVariable], 
-                    clabel=colorScaleVariable,
-                    cmap='viridis', 
-                    tiles='StamenTerrain', 
-                    title='Back Trajectories for Raven 2024:' + colorScaleVariable)
-# %%
